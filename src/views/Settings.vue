d5b32700129fa200f66ff618cb899c45c8d62283
Adam Costanza <adamco@zillowgroup.com>
2022-02-06 15:22:06 -0700

    settings

--- src/views/Settings.vue
+++ src/views/Settings.vue
@@ -5,19 +5,11 @@
       <v-toolbar-title>Settings</v-toolbar-title>
     </v-toolbar>
     <v-card>
-      <v-btn
-        color="success"
-        class="mr-4"
-        @click="addNewMaterial"
-        style="margin: 18px"
+      <v-btn color="success" class="mr-4" @click="download" style="margin: 18px"
         ><v-icon>mdi-download</v-icon>Download Settings File</v-btn
       >
-      <v-btn
-        color="success"
-        class="mr-4"
-        @click="addNewMaterial"
-        style="margin: 18px"
-      ><v-icon>mdi-upload</v-icon>Upload Settings File</v-btn
+      <v-btn color="success" class="mr-4" @click="upload" style="margin: 18px"
+        ><v-icon>mdi-upload</v-icon>Upload Settings File</v-btn
       >
     </v-card>
   </div>
@@ -29,5 +21,27 @@ import { DefaultMaterials, Material } from "@/utils/material";
 import MaterialForm from "@/components/MaterialForm.vue";
 
 @Component
-export default class Settings extends Vue {}
+export default class Settings extends Vue {
+  download() {
+    const keys = ["machine", "cutters", "materials"];
+    const data = keys.reduce(
+      (acc, key) => ({ ...acc, [key]: localStorage.getItem(key) }),
+      {}
+    );
+
+    this.downloadAsFile(
+      JSON.stringify(data),
+      "feeds-and-speeds.json",
+      "text/plain"
+    );
+  }
+
+  downloadAsFile(content: string, fileName: string, contentType: string) {
+    const a = document.createElement("a");
+    const file = new Blob([content], { type: contentType });
+    a.href = URL.createObjectURL(file);
+    a.download = fileName;
+    a.click();
+  }
+}
 </script>
