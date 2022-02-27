aa599d4e5dd8ce130d22ddbabe6d9b4f63ce7235
Adam Costanza <adamco@zillowgroup.com>
2022-02-27 14:54:19 -0700

    deploy

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,12 +1,3 @@
-BRANCH=$(git rev-parse --abbrev-ref HEAD)
-git stash
-git branch -D gh-pages
-git checkout -b gh-pages
-npm install
 npm run build
-find ./ -mindepth 1 ! -regex '^./dist\(/.*\)?' -delete
-cp -Rf dist/. .
-rm -Rf dist
-git push -u origin gh-pages --force
-git checkout $BRANCH
-git stash pop
+git add dist && git commit -m 'adding dist subtree'
+git subtree push --prefix dist origin gh-pages
