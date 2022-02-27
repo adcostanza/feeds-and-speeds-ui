f3b98de008915a02255394ff7d6c4095804477e2
Adam Costanza <adamco@zillowgroup.com>
2022-02-27 14:42:23 -0700

    deploy

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,3 +1,4 @@
+BRANCH=$(git rev-parse --abbrev-ref HEAD)
 git stash
 git branch -D gh-pages
 git checkout -b gh-pages
@@ -7,3 +8,5 @@ find ./ -mindepth 1 ! -regex '^./dist\(/.*\)?' -delete
 cp -Rf dist/. .
 rm -Rf dist
 git push -u origin gh-pages --force
+git checkout $BRANCH
+git stash pop
