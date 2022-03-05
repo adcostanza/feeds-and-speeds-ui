46faac7b66bc5ce9b1fd7a3f9524c0b391b92c83
Adam Costanza <adamco@zillowgroup.com>
2022-03-05 15:02:27 -0700

    fix?

--- src/utils/optimization.ts
+++ src/utils/optimization.ts
@@ -98,7 +98,7 @@ export const executeOptimization = ({
           (acc, [key, fn]) => {
             return { ...acc, [key]: fromPositionalToNamed(fn, inputs) };
           },
-          { ...inputs }
+          {}
         );
 
         let constraintFulfilled = true;
@@ -109,7 +109,12 @@ export const executeOptimization = ({
           }
         }
 
-        const resultWithMetadata = { ...result, constraintFulfilled, count };
+        const resultWithMetadata = {
+          ...inputs,
+          ...result,
+          constraintFulfilled,
+          count,
+        };
         count++;
         return resultWithMetadata;
       });
